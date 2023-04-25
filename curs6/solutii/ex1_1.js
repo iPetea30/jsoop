e983ec7e031d422e4dedb485bc787a29d5f46882
iPetea30 <dumi.puia@gmail.com>
2023-04-25 18:00:44 +0300

    curs6

---
+++ curs6/solutii/ex1_1.js
@@ -0,0 +1,12 @@
+console.log(process.argv);
+
+var sum = 0;
+for (i = 2; i < process.argv.length; i++) {
+    sum += Number(process.argv[i]);
+}
+console.log(sum);
+
+
+// The process.argv property returns an array containing the command-line arguments 
+// process.argv[0] - path/to/node.exe
+// process.argv[1] - path/to/js/file 
